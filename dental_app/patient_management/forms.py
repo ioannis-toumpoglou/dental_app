bbb556b9a96dc37db96fae553beb51074757a359
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-01-28 00:53:10 +0200

    Make certain form fields optional, add textarea for notes

--- dental_app/patient_management/forms.py
+++ dental_app/patient_management/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.conf import settings
 
 
 class PatientForm(forms.Form):
     first_name = forms.CharField(label='First Name:')
     last_name = forms.CharField(label='Last Name:')
-    address = forms.CharField(label='Address:')
-    email = forms.EmailField(label='Email:')
-    phone = forms.CharField(label='Phone number:')
-    cell_phone = forms.CharField(label='Cell phone:')
-    amka = forms.CharField(label='AMKA:')
-    date_of_birth = forms.DateField(label='Date of birth:')
+    address = forms.CharField(label='Address:', required=False)
+    email = forms.EmailField(label='Email:', required=False)
+    phone = forms.CharField(label='Phone number:', required=False)
+    mobile_phone = forms.CharField(label='Mobile phone:')
+    amka = forms.CharField(label='AMKA:', required=False)
+    date_of_birth = forms.DateField(label='Date of birth:', input_formats=settings.DATE_INPUT_FORMATS, required=False)
+    notes = forms.CharField(widget=forms.Textarea(attrs={"rows": "5"}))
