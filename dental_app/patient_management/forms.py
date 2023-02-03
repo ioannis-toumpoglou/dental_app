7f3ceb6c1fa663e5d2c3657c8266421c9a279050
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-02-04 00:17:46 +0200

    Adapt the patient form to use patient model's attributes as fields

--- dental_app/patient_management/forms.py
+++ dental_app/patient_management/forms.py
@@ -1,13 +1,9 @@
 from django import forms
 
+from .models import Patient
 
-class PatientForm(forms.Form):
-    first_name = forms.CharField(label='First Name:')
-    last_name = forms.CharField(label='Last Name:')
-    address = forms.CharField(label='Address:', required=False)
-    email = forms.EmailField(label='Email:', required=False)
-    phone = forms.CharField(label='Phone number:', required=False)
-    mobile_phone = forms.CharField(label='Mobile phone:')
-    amka = forms.CharField(label='AMKA:', required=False)
-    date_of_birth = forms.DateField(label='Date of birth:', required=False)
-    notes = forms.CharField(widget=forms.Textarea(attrs={"rows": "5"}), required=False)
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = '__all__'
