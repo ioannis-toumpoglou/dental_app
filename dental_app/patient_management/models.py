0761c363cd04c593d3a6c188e2a9a4f38119d3b8
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-02-04 00:17:02 +0200

    Add id attribute to patient model and define optional attributes

--- dental_app/patient_management/models.py
+++ dental_app/patient_management/models.py
@@ -3,14 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=100, blank=True)
     mobile_phone = models.CharField(max_length=100)
-    amka = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
+    amka = models.CharField(max_length=100, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    notes = models.CharField(max_length=500, null=True, blank=True)
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
