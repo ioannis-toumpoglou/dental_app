a961a8639fa08ebb6b5e6eb447e209fc3d73e3b1
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-02-06 12:50:57 +0200

    Add patient model in admin console

--- dental_app/patient_management/admin.py
+++ dental_app/patient_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Patient
+
+
 # Register your models here.
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'address', 'email', 'phone', 'mobile_phone', 'amka', 'date_of_birth')
+
+
+admin.site.register(Patient, PatientAdmin)
