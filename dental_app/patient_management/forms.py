b1fa820195747a7666bc14bedd06f1d4ee6d783b
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-02-07 00:43:10 +0200

    Improve the app styling

--- dental_app/patient_management/forms.py
+++ dental_app/patient_management/forms.py
@@ -7,3 +7,5 @@ class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
         fields = '__all__'
+        widgets = {
+            'notes': forms.Textarea(attrs={'rows':5}),}
