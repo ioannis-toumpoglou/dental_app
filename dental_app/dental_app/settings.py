fc1163f4fd984a16a420205880ce56c64f457e36
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-02-07 23:29:46 +0200

    Use template inheritance

--- dental_app/dental_app/settings.py
+++ dental_app/dental_app/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'dental_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
