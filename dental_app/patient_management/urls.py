d6eabacdfdb77fa52cb8a0cd1ec664ddfea67e86
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-03-12 23:41:57 +0200

    Remove path for main page

--- dental_app/patient_management/urls.py
+++ dental_app/patient_management/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/', views.main, name='main'),
+    path('', views.main, name='main'),
     path('add-patient/', views.AddPatientFormView.as_view(), name='add-patient'),
     path('patient-details/<int:patient_id>', views.edit_patient, name='patient-details')
 ]
