e49612de1875cee5d5f803abce90ee0e8d833951
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-03-28 00:55:38 +0300

    Change date format

--- dental_app/dental_app/settings.py
+++ dental_app/dental_app/settings.py
@@ -145,3 +145,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Format date
+DATE_FORMAT = 'd-m-Y'
+USE_L10N = False
