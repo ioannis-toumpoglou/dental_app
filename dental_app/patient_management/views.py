64fca958b3b4b68cb92842d799a9f4fabdcfa89f
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2023-03-27 03:31:19 +0300

    Implement appointment edit functionality

--- dental_app/patient_management/views.py
+++ dental_app/patient_management/views.py
@@ -3,8 +3,8 @@ from django.views.generic.edit import FormView
 from pymongo import MongoClient
 from django.db.models import Q
 
-from .forms import PatientForm, MedicalHistoryForm, DentalHistoryForm
-from .models import Patient, MedicalHistory, DentalHistory
+from .forms import PatientForm, MedicalHistoryForm, DentalHistoryForm, AppointmentForm
+from .models import Patient, MedicalHistory, DentalHistory, Appointment
 
 
 # Create your views here.
@@ -59,13 +59,22 @@ def edit_patient(request, patient_id):
     patient_form = PatientForm(instance=patient)
 
     medical_history = MedicalHistory.objects.filter(patient=patient_id).first()
+
     if medical_history is None:
         medical_history = MedicalHistory.objects.create(patient_id=patient_id)
     medical_history_form = MedicalHistoryForm(instance=medical_history)
+
     dental_history = DentalHistory.objects.filter(patient=patient_id).first()
+
     if dental_history is None:
         dental_history = DentalHistory.objects.create(patient_id=patient_id)
     dental_history_form = DentalHistoryForm(instance=dental_history)
+
+    appointments_form_list = get_appointments_form_list(patient_id=patient_id)
+
+    context = {}
+    appointment_form = AppointmentForm(request.POST)
+    context['appointment_form'] = appointment_form
 
     if request.method == 'POST':
         if 'delete' in request.POST:
@@ -76,44 +85,103 @@ def edit_patient(request, patient_id):
             medical_history.patient = patient
             medical_history_form = MedicalHistoryForm(request.POST, instance=medical_history)
             medical_history_form.save()
-            return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
                                                                                'medical_form': medical_history_form,
-                                                                               'dental_form': dental_history_form})
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
 
         if 'clear-medical' in request.POST:
             medical_history = MedicalHistory.objects.get(patient_id=patient_id)
             medical_history.delete()
             medical_history = MedicalHistory.objects.create(patient_id=patient_id)
             medical_history_form = MedicalHistoryForm(instance=medical_history)
-            return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
                                                                                'medical_form': medical_history_form,
-                                                                               'dental_form': dental_history_form})
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
 
         if 'save-dental' in request.POST:
             dental_history.patient = patient
             dental_history_form = DentalHistoryForm(request.POST, instance=dental_history)
             dental_history_form.save()
-            return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
                                                                                'medical_form': medical_history_form,
-                                                                               'dental_form': dental_history_form})
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
 
         if 'clear-dental' in request.POST:
             dental_history = DentalHistory.objects.get(patient_id=patient_id)
             dental_history.delete()
             dental_history = DentalHistory.objects.create(patient_id=patient_id)
             dental_history_form = DentalHistoryForm(instance=dental_history)
-            return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
                                                                                'medical_form': medical_history_form,
-                                                                               'dental_form': dental_history_form})
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
+
+        if 'save-appointment' in request.POST:
+            appointment = Appointment.objects.create(patient_id=patient_id)
+            appointment.patient = patient
+            appointment_form = AppointmentForm(request.POST, instance=appointment)
+            appointment_form.save()
+            appointments_form_list = get_appointments_form_list(patient_id=patient_id)
+            appointment_form = AppointmentForm()
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
+                                                                               'medical_form': medical_history_form,
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
+
+        if 'edit-appointment' in request.POST:
+            appointment_id = request.POST.get('id')
+            edited_appointment = Appointment.objects.get(id=appointment_id)
+            edited_appointment.notes = request.POST.get('notes')
+            edited_appointment.save()
+            appointment_form = AppointmentForm()
+
+            appointments_form_list = get_appointments_form_list(patient_id=patient_id)
+
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
+                                                                               'medical_form': medical_history_form,
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
 
         patient_form = PatientForm(request.POST, instance=patient)
 
         if patient_form.is_valid():
             patient_form.save()
-            return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+            return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                               'form': patient_form,
                                                                                'medical_form': medical_history_form,
-                                                                               'dental_form': dental_history_form})
+                                                                               'dental_form': dental_history_form,
+                                                                               'appointment_form': appointment_form,
+                                                                               'appointments_form_list': appointments_form_list})
 
-    return render(request, 'patient_management/patient-details.html', {'form': patient_form,
+    return render(request, 'patient_management/patient-details.html', {'patient': patient,
+                                                                       'form': patient_form,
                                                                        'medical_form': medical_history_form,
-                                                                       'dental_form': dental_history_form})
+                                                                       'dental_form': dental_history_form,
+                                                                       'appointment_form': appointment_form,
+                                                                       'appointments_form_list': appointments_form_list})
+
+
+def get_appointments_form_list(patient_id):
+    appointments_list = Appointment.objects.filter(patient_id=patient_id)
+    appointments_form_list = list()
+
+    for appointment in appointments_list:
+        appointment_form = AppointmentForm(auto_id=appointment.id, instance=appointment)
+        appointments_form_list.append(appointment_form)
+
+    return appointments_form_list
