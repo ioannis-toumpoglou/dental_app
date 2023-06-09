fb645a80bb7b5232e5c071136e0acc4c75af662d
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2023-06-09 12:17:28 +0300

    Add calendar path

--- dental_app/patient_management/urls.py
+++ dental_app/patient_management/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('add-patient/', views.AddPatientFormView.as_view(), name='add-patient'),
-    path('patient-details/<int:patient_id>', views.edit_patient, name='patient-details')
+    path('patient-details/<int:patient_id>', views.edit_patient, name='patient-details'),
+    path('calendar/', views.CalendarView.as_view(), name='calendar')
 ]
