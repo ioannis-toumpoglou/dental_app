893fe7b912a08982880d705c9c0ce9299331723d
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2023-06-20 14:34:18 +0300

    Add edit time option for booked patient appointments

--- dental_app/patient_management/views.py
+++ dental_app/patient_management/views.py
@@ -149,6 +149,8 @@ def edit_patient(request, patient_id):
         if 'edit-appointment' in request.POST:
             appointment_id = request.POST.get('id')
             edited_appointment = Appointment.objects.get(id=appointment_id)
+            edited_appointment.appointment_time = request.POST.get('appointment_time')
+            edited_appointment.appointment_time = request.POST.get('appointment_time')
             edited_appointment.notes = request.POST.get('notes')
             edited_appointment.save()
             appointment_form = AppointmentForm()
