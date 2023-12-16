17fad3e266c4d69c7c8e7e9e6fce933f51ecd687
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2023-12-16 10:52:06 +0200

    Implement the forms for the periodontogram teeth input

--- dental_app/patient_management/forms.py
+++ dental_app/patient_management/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from .models import Patient, MedicalHistory, DentalHistory, Appointment, TreatmentPlan, Financial, Odontogram
+from .models import (Patient, MedicalHistory, DentalHistory, Appointment, TreatmentPlan, Financial, Odontogram,
+                     Periodontogram)
 
 
 class PatientForm(forms.ModelForm):
@@ -448,6 +449,460 @@ class Tooth48Form(forms.ModelForm):
     class Meta:
         model = Odontogram
         fields = ['tooth_48', ]
+
+
+PERIO_CHOICES = (
+    ('minor', 'minor'),
+    ('major', 'major'),
+)
+
+
+class PerioTooth11Form(forms.ModelForm):
+    perio_tooth_11 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_11', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth12Form(forms.ModelForm):
+    perio_tooth_12 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_12', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth13Form(forms.ModelForm):
+    perio_tooth_13 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_13', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth14Form(forms.ModelForm):
+    perio_tooth_14 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_14', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth15Form(forms.ModelForm):
+    perio_tooth_15 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_15', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth16Form(forms.ModelForm):
+    perio_tooth_16 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_16', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth17Form(forms.ModelForm):
+    perio_tooth_17 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_17', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth18Form(forms.ModelForm):
+    perio_tooth_18 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_18', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth21Form(forms.ModelForm):
+    perio_tooth_21 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_21', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth22Form(forms.ModelForm):
+    perio_tooth_22 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_22', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth23Form(forms.ModelForm):
+    perio_tooth_23 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_23', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth24Form(forms.ModelForm):
+    perio_tooth_24 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_24', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth25Form(forms.ModelForm):
+    perio_tooth_25 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_25', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth26Form(forms.ModelForm):
+    perio_tooth_26 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_26', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth27Form(forms.ModelForm):
+    perio_tooth_27 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_27', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth28Form(forms.ModelForm):
+    perio_tooth_28 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_28', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth31Form(forms.ModelForm):
+    perio_tooth_31 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_31', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth32Form(forms.ModelForm):
+    perio_tooth_32 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_32', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth33Form(forms.ModelForm):
+    perio_tooth_33 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_33', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth34Form(forms.ModelForm):
+    perio_tooth_34 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_34', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth35Form(forms.ModelForm):
+    perio_tooth_35 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_35', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth36Form(forms.ModelForm):
+    perio_tooth_36 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_36', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth37Form(forms.ModelForm):
+    perio_tooth_37 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_37', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth38Form(forms.ModelForm):
+    perio_tooth_38 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_38', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth41Form(forms.ModelForm):
+    perio_tooth_41 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_41', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth42Form(forms.ModelForm):
+    perio_tooth_42 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_42', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth43Form(forms.ModelForm):
+    perio_tooth_43 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_43', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth44Form(forms.ModelForm):
+    perio_tooth_44 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_44', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth45Form(forms.ModelForm):
+    perio_tooth_45 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_45', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth46Form(forms.ModelForm):
+    perio_tooth_46 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_46', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth47Form(forms.ModelForm):
+    perio_tooth_47 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_47', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
+
+
+class PerioTooth48Form(forms.ModelForm):
+    perio_tooth_48 = forms.MultipleChoiceField(
+        choices=PERIO_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Periodontogram
+        fields = ['perio_tooth_48', ]
+        widgets = {
+            'perio_notes': forms.Textarea(attrs={'style': 'width: 100%;', 'rows': '3'})}
 
 
 class FinancialForm(forms.ModelForm):
